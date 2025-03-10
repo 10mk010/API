436a42377e4247e26b66a9ed8e0d1c912686f789
10mk010 <163329405+10mk010@users.noreply.github.com>
2025-03-10 15:16:55 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-'use strict';
-
+"use strict";
+/*
 const container = document.getElementById("container");
 const button = document.createElement("button");
 button.innerText = "Show Photo!";
@@ -15,4 +15,38 @@ button.addEventListener("click", async function getAPIData(){
     img.src = data.file;
     img.alt = "Coffee Photo";
     container.appendChild(img);
+})
+*/
+
+const countryName = document.getElementById("countryName");
+const searchBtn = document.getElementById("searchBtn");
+
+searchBtn.addEventListener("click", async (e) => {
+    e.preventDefault();
+    const inputValue = countryName.value.trim();
+    // https://restcountries.com/v3.1/name/{name}
+    let data;
+    try {
+        const response = await fetch(`https://restcountries.com/v3.1/name/${inputValue}`);
+        data = await response.json();
+    } catch (err) {
+        console.log(err); 
+        return;
+    }
+    if(data.)
+    const result = data[0];
+    const flag = result.flags.svg;
+    const name = result.name.common;
+    const capital = result.capital;
+
+    const card = document.createElement("div");
+    card.classList.add("card");
+    const flagImg = document.createElement("img");
+    flagImg.src = flag;
+    const nameheader = document.createElement("h1");
+    nameheader.innerText = name;
+    const capitalP = document.createElement("p");
+    capitalP.innerText = capital;
+    document.getElementById("results").appendChild(card);
+    card.append(nameheader, flagImg, capitalP);
 })
